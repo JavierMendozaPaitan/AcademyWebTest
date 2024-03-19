e332f7b6bc6f1bdfccb4b89d1d455b7559f8cc8d
Javier Mendoza <Javier_Mendoza@epam.com>
2024-03-19 20:08:49 +0100

    implementing services and web api

---
+++ AcademyWebWorkshop/AcademyWeb.Engine/StudentService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AcademyWeb.Abstractions;
+using AcademyWeb.Models;
+using DataProvider.Abstractions;
+
+namespace AcademyWeb.Engine
+{
+    public class StudentService : IStudentService
+    {
+        private readonly IStudentRepository _studentRepository;
+        public StudentService(
+            IStudentRepository studentRepository) 
+        {
+            _studentRepository = studentRepository;
+        }
+        public List<StudentAcademy> GetStudentsInTraining()
+        {
+            var students = new List<StudentAcademy>();
+            var list = _studentRepository.GetStudents().Where(x => x.StudentLevelId == 2);
+            foreach (var item in list)
+            {
+                var student = new StudentAcademy
+                {
+                    Id = item.Id,
+                    Name = item.StudentName,
+                    Email = item.Email,
+                    StudentLevel = "Training"
+                };
+                students.Add(student);
+            }
+
+            return students;
+        }
+    }
+}
---
+++ AcademyWebWorkshop/AcademyWebb.Models/StudentAcademy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcademyWeb.Models
+{
+    public class StudentAcademy
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? StudentLevel { get; set; }
+    }
+}
